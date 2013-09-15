a783996396ddac08ff4fee008006232ae0d85374
anarkia1976 <stefano.villa1976@gmail.com>
2013-09-15 16:30:11 +0200

    First Commit Anykernel Format

---
+++ cwm/kernel/repack-ramdisk.sh
@@ -0,0 +1,43 @@
+#!/sbin/sh
+mkdir /tmp/ramdisk
+cd /tmp/ramdisk
+gunzip -c ../boot.img-ramdisk.gz | cpio -i
+
+#add init.d support if not already supported
+found=$(find init.rc -type f | xargs grep -oh "run-parts /system/etc/init.d");
+if [ "$found" != 'run-parts /system/etc/init.d' ]; then
+        #find busybox in /system
+        bblocation=$(find /system/ -name 'busybox')
+        if [ -n "$bblocation" ] && [ -e "$bblocation" ] ; then
+                echo "BUSYBOX FOUND!";
+                #strip possible leading '.'
+                bblocation=${bblocation#.};
+        else
+                echo "NO BUSYBOX NOT FOUND! init.d support will not work without busybox!";
+                echo "Setting busybox location to /system/xbin/busybox! (install it and init.d will work)";
+                #set default location since we couldn't find busybox
+                bblocation="/system/xbin/busybox";
+        fi
+		#append the new lines for this option at the bottom
+        echo "" >> init.rc
+        echo "service userinit $bblocation run-parts /system/etc/init.d" >> init.rc
+        echo "    oneshot" >> init.rc
+        echo "    class late_start" >> init.rc
+        echo "    user root" >> init.rc
+        echo "    group root" >> init.rc
+fi
+
+#remove governor overrides, use kernel default
+#sed -i '/\/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_governor/d' init.mako.rc
+#sed -i '/\/sys\/devices\/system\/cpu\/cpu1\/cpufreq\/scaling_governor/d' init.mako.rc
+#sed -i '/\/sys\/devices\/system\/cpu\/cpu2\/cpufreq\/scaling_governor/d' init.mako.rc
+#sed -i '/\/sys\/devices\/system\/cpu\/cpu3\/cpufreq\/scaling_governor/d' init.mako.rc
+
+# Remove mpdecision and thermald
+#sed -i '/mpdecision/{n; /class main$/d}' init.mako.rc
+#sed -i '/thermald/{n; /class main$/d}' init.mako.rc
+#sed -i '/mpdecision/d' init.mako.rc
+#sed -i '/thermald/d' init.mako.rc
+
+find . | cpio -o -H newc | gzip > ../newramdisk.cpio.gz
+cd /
