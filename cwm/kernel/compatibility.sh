83230c308a1d0f85047f9887fb45d212348ec27f
anarkia1976 <stefano.villa1976@gmail.com>
2014-01-06 09:57:45 +0100

    compatibility: make a backup of power.msm8960.so

--- cwm/kernel/compatibility.sh
+++ cwm/kernel/compatibility.sh
@@ -9,6 +9,11 @@ if [ -e /system/bin/thermald ] ; then
 	busybox mv /system/bin/thermald /system/bin/thermald_bck
 fi
 
+# remove the library as they are no longer needed. (kernel handled)
+if [ -e /system/lib/hw/power.msm8960.so ] ; then
+        busybox mv /system/lib/hw/power.msm8960.so /system/lib/hw/power.msm8960.so_bck
+fi
+
 # backup the old prima stuff
 if [ ! -e /system/vendor/firmware/wlan/prima/WCNSS_cfg.dat_bck ] ; then
         busybox mv /system/vendor/firmware/wlan/prima/WCNSS_cfg.dat /system/vendor/firmware/wlan/prima/WCNSS_cfg.dat_bck
