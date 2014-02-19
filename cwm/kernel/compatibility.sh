ca0080abe2456360ec07996f74bf5b4f6631920b
anarkia1976 <stefano.villa1976@gmail.com>
2014-02-19 06:40:41 +0100

    prima: revert to default drivers

--- cwm/kernel/compatibility.sh
+++ cwm/kernel/compatibility.sh
@@ -15,10 +15,10 @@ if [ -e /system/lib/hw/power.msm8960.so ] ; then
 fi
 
 # backup the old prima stuff
-if [ ! -e /system/vendor/firmware/wlan/prima/WCNSS_cfg.dat_bck ] ; then
-        busybox mv /system/vendor/firmware/wlan/prima/WCNSS_cfg.dat /system/vendor/firmware/wlan/prima/WCNSS_cfg.dat_bck
-fi
-if [ ! -e /system/etc/wifi/WCNSS_qcom_cfg.ini_bck ] ; then
-        busybox mv /system/etc/wifi/WCNSS_qcom_cfg.ini /system/etc/wifi/WCNSS_qcom_cfg.ini_bck
-fi
+#if [ ! -e /system/vendor/firmware/wlan/prima/WCNSS_cfg.dat_bck ] ; then
+#        busybox mv /system/vendor/firmware/wlan/prima/WCNSS_cfg.dat /system/vendor/firmware/wlan/prima/WCNSS_cfg.dat_bck
+#fi
+#if [ ! -e /system/etc/wifi/WCNSS_qcom_cfg.ini_bck ] ; then
+#        busybox mv /system/etc/wifi/WCNSS_qcom_cfg.ini /system/etc/wifi/WCNSS_qcom_cfg.ini_bck
+#fi
 return $?
